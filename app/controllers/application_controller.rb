85bc9b10dfff016172917ae1b92fd2f05ff1becf
chihirosugimoto <kasugaichihiro@i.softbank.jp>
2022-06-10 22:18:41 +0900

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :login_required
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:uer_id]) if session[:user_id]
+  end
+    
+
 end
+
